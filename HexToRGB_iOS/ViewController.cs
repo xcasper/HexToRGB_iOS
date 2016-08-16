392bf822ca9c09810eb0b8390c7022661cd51cbf
Craig <cgleckman@gmail.com>
2016-08-15 20:50:13 -0700

    initial commit + final product
    forgot to initial commit.. but this is the final product

---
+++ HexToRGB_iOS/ViewController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+using UIKit;
+
+namespace HexToRGB_iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            convertButton.TouchUpInside += ConvertButton_TouchUpInside1;
+        }
+
+        private void ConvertButton_TouchUpInside1(object sender, EventArgs e)
+        {
+            /*string hexValue = hexTextField.Text;
+            string redHexValue = hexValue.Substring(0, 2);
+            string greenHexValue = hexValue.Substring(2, 2);
+            string blueHexValue = hexValue.Substring(4, 2);
+
+            int redValue = int.Parse(redHexValue, System.Globalization.NumberStyles.HexNumber);
+            int greenValue = int.Parse(greenHexValue, System.Globalization.NumberStyles.HexNumber);
+            int blueValue = int.Parse(blueHexValue, System.Globalization.NumberStyles.HexNumber);
+
+            redLabel.Text = redValue.ToString();
+            greenLabel.Text = greenValue.ToString();
+            blueLabel.Text = blueValue.ToString();
+
+            // colorView.BackgroundColor = UIColor.FromRGB(redValue, greenValue, blueValue); */
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void ConvertButton_TouchUpInside(UIButton sender)
+        {
+            string hexValue = hexTextField.Text;
+            errorLabel.Text = "";
+            try
+            {
+                string redHexValue = hexValue.Substring(0, 2);
+                string greenHexValue = hexValue.Substring(2, 2);
+                string blueHexValue = hexValue.Substring(4, 2);
+
+                int redValue = int.Parse(redHexValue, System.Globalization.NumberStyles.HexNumber);
+                int greenValue = int.Parse(greenHexValue, System.Globalization.NumberStyles.HexNumber);
+                int blueValue = int.Parse(blueHexValue, System.Globalization.NumberStyles.HexNumber);
+
+                redLabel.Text = redValue.ToString();
+                greenLabel.Text = greenValue.ToString();
+                blueLabel.Text = blueValue.ToString();
+
+                colorView.BackgroundColor = UIColor.FromRGB(redValue, greenValue, blueValue);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                //les then 6 characters entered
+                errorLabel.Text = "Please enter 6 digits";
+            }
+            catch (FormatException)
+            {
+                //wrong format... valid values: 0-9 A-F
+                errorLabel.Text = "Please enter proper values. Valid values are 0-9 and A-F";
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ HexToRGB_iOS/ViewController.designer.cs
@@ -0,0 +1,87 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace HexToRGB_iOS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel blueLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIView colorView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton convertButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel errorLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel greenLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField hexTextField { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel redLabel { get; set; }
+
+        [Action ("ConvertButton_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void ConvertButton_TouchUpInside (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (blueLabel != null) {
+                blueLabel.Dispose ();
+                blueLabel = null;
+            }
+
+            if (colorView != null) {
+                colorView.Dispose ();
+                colorView = null;
+            }
+
+            if (convertButton != null) {
+                convertButton.Dispose ();
+                convertButton = null;
+            }
+
+            if (errorLabel != null) {
+                errorLabel.Dispose ();
+                errorLabel = null;
+            }
+
+            if (greenLabel != null) {
+                greenLabel.Dispose ();
+                greenLabel = null;
+            }
+
+            if (hexTextField != null) {
+                hexTextField.Dispose ();
+                hexTextField = null;
+            }
+
+            if (redLabel != null) {
+                redLabel.Dispose ();
+                redLabel = null;
+            }
+        }
+    }
+}
